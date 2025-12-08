572838472121d629978c9655a35914f281e8da4d
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-12-08 23:20:00 +0500

    add suspanse 2 pages

--- src/app/learn-form/page.jsx
+++ src/app/learn-form/page.jsx
@@ -2,8 +2,9 @@
 
 import LearnForm from "@/components/school-form/LearnForm";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function LearnFormpage() {
+function LearnFormWrapper() {
   const params = useSearchParams();
   const utm = params.get("utm_source") || "unknown";
 
@@ -11,5 +12,13 @@ export default function LearnFormpage() {
     <div className="w-full h-screen flex items-center justify-center mx-auto py-10 px-4 space-y-6">
       <LearnForm utm={utm} />
     </div>
+  );
+}
+
+export default function LearnFormPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LearnFormWrapper />
+    </Suspense>
   );
 }
--- src/app/school-form/page.jsx
+++ src/app/school-form/page.jsx
@@ -2,8 +2,9 @@
 
 import { useSearchParams } from "next/navigation";
 import SchoolForm from "@/components/school-form/SchoolForm";
+import { Suspense } from "react";
 
-export default function SchoolFormpage() {
+function SchoolFormWrapper() {
   const params = useSearchParams();
   const utm = params.get("utm_source") || "unknown";
 
@@ -11,5 +12,13 @@ export default function SchoolFormpage() {
     <div className="w-full h-screen flex items-center justify-center mx-auto py-10 px-4 space-y-6">
       <SchoolForm utm={utm} />
     </div>
+  );
+}
+
+export default function SchoolFormPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SchoolFormWrapper />
+    </Suspense>
   );
 }
