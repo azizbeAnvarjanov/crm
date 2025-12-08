8606ee57341cac4e893fb9b8772e2da369f42adf
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-12-08 23:17:54 +0500

    add suspanse

--- src/app/kind-form/page.jsx
+++ src/app/kind-form/page.jsx
@@ -2,8 +2,9 @@
 
 import KindergartenForm from "@/components/school-form/KindergartenForm";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function KindergartenFormPage() {
+function FormWrapper() {
   const params = useSearchParams();
   const utm = params.get("utm_source") || "unknown";
 
@@ -11,5 +12,13 @@ export default function KindergartenFormPage() {
     <div className="w-full h-screen flex items-center justify-center mx-auto py-10 px-4 space-y-6">
       <KindergartenForm utm={utm} />
     </div>
+  );
+}
+
+export default function KindergartenFormPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <FormWrapper />
+    </Suspense>
   );
 }
